6625ee49103d828e7bb4e9953eda87354b103cdb
stephen <stephen.korzen@gmail.com>
2026-02-01 11:33:22 +0000

    Changes before error encountered

--- crates/grift_parser/src/macros.scm
+++ crates/grift_parser/src/macros.scm
@@ -4,6 +4,63 @@
 ;;; macro-based implementations of common forms.
 
 ;; ============================================================
+;; Binding Forms (R7RS Section 4.2.2)
+;; ============================================================
+
+;; let - basic binding form
+;; Regular: (let ((var val) ...) body ...)
+;; Named: (let name ((var val) ...) body ...) for iteration
+;; Note: Regular let must come first because named let's 'name' would
+;; match any expression including the binding list
+(define-syntax let
+  (syntax-rules ()
+    ;; Regular let: (let ((var val) ...) body ...)
+    ((let ((var val) ...) body1 body2 ...)
+     ((lambda (var ...) body1 body2 ...) val ...))
+    ;; Named let: (let name ((var val) ...) body ...)
+    ((let name ((var val) ...) body1 body2 ...)
+     (letrec ((name (lambda (var ...) body1 body2 ...)))
+       (name val ...)))))
+
+;; let* - sequential binding form
+(define-syntax let*
+  (syntax-rules ()
+    ((let* () body1 body2 ...)
+     (let () body1 body2 ...))
+    ((let* ((name1 val1) (name2 val2) ...)
+       body1 body2 ...)
+     (let ((name1 val1))
+       (let* ((name2 val2) ...)
+         body1 body2 ...)))))
+
+;; letrec - recursive binding form
+;; We use begin to sequence the set! calls and body
+(define-syntax letrec
+  (syntax-rules ()
+    ((letrec () body1 body2 ...)
+     (let () body1 body2 ...))
+    ((letrec ((var1 init1)) body1 body2 ...)
+     (let ((var1 #f))
+       (set! var1 init1)
+       (let () body1 body2 ...)))
+    ((letrec ((var1 init1) (var2 init2) ...) body1 body2 ...)
+     (let ((var1 #f))
+       (set! var1 init1)
+       (letrec ((var2 init2) ...) body1 body2 ...)))))
+
+;; letrec* - sequential recursive binding form
+(define-syntax letrec*
+  (syntax-rules ()
+    ((letrec* () body1 body2 ...)
+     (let () body1 body2 ...))
+    ((letrec* ((var1 init1) (var2 init2) ...)
+       body1 body2 ...)
+     (let ((var1 #f))
+       (set! var1 init1)
+       (letrec* ((var2 init2) ...)
+         body1 body2 ...)))))
+
+;; ============================================================
 ;; Conditionals
 ;; ============================================================
 
@@ -50,6 +107,47 @@
      (if test (begin result1 result2 ...) (cond rest ...)))
     ((cond)
      #f)))
+
+;; ============================================================
+;; Pattern Matching (case)
+;; ============================================================
+
+;; case - Pattern matching on datum values
+(define-syntax case
+  (syntax-rules (else)
+    ((case key (else result1 result2 ...))
+     (begin result1 result2 ...))
+    ((case key ((atoms ...) result1 result2 ...))
+     (if (memv key '(atoms ...))
+         (begin result1 result2 ...)))
+    ((case key ((atoms ...) result1 result2 ...) clause ...)
+     (if (memv key '(atoms ...))
+         (begin result1 result2 ...)
+         (case key clause ...)))))
+
+;; ============================================================
+;; Iteration (do)
+;; ============================================================
+
+;; do - General iteration construct
+;; Helper macro for extracting step expression
+(define-syntax do-step
+  (syntax-rules ()
+    ((do-step var) var)
+    ((do-step var step) step)))
+
+;; do - (do ((var init step) ...) (test result ...) body ...)
+(define-syntax do
+  (syntax-rules ()
+    ((do ((var init step ...) ...)
+         (test result ...)
+         body ...)
+     (let loop ((var init) ...)
+       (if test
+           (begin (if #f #f) result ...)
+           (begin
+             body ...
+             (loop (do-step var step ...) ...)))))))
 
 ;; ============================================================
 ;; Delayed Evaluation
