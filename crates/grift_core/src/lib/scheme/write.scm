9f6c1ec9fb2f75b2c484b1d74de18bc033bfee25
gold_silver_copper <gold_silver_copper@example.invalid>
2026-02-10 02:51:38 -0800

    Update write.scm

--- crates/grift_core/src/lib/scheme/write.scm
+++ crates/grift_core/src/lib/scheme/write.scm
@@ -1,4 +1,5 @@
 ;;; (scheme write) — R7RS §6.13.3
 (define-library (scheme write)
   (export write display
-          write-shared write-simple))
+          write-shared write-simple
+          write-char write-string newline))
